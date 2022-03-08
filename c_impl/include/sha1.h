551c431aaf06c655c4bbdf3c47a7222c9d731bab
Øyvind Skaaden <oyvindskaaden@gmail.com>
2022-03-08 15:15:40 +0100

    added dbg print

--- c_impl/include/sha1.h
+++ c_impl/include/sha1.h
@@ -12,16 +12,23 @@
 
 #define MINIMUM_TAIL_BYTES  9   /* 1 byte for 0x80 and 8 bytes for the 64 bit data length in bits */
 
-#define rol(value, bits) (((value) << (bits)) | ((value) >> (32 - (bits))))
-#define ror(value, bits) (((value) >> (bits)) | ((value) << (32 - (bits))))
+#define rol(value, bits)    (((value) << (bits)) | ((value) >> (32 - (bits))))
+#define ror(value, bits)    (((value) >> (bits)) | ((value) << (32 - (bits))))
 
-#define big_endian(value) (((value & 0xff) << 24) | (((value & 0xff00)) << 8) | (((value & 0xff0000)) >> 8) | ((value & 0xff000000) >> 24))
+#define big_endian(value)   (((value & 0xff) << 24) | (((value & 0xff00)) << 8) | (((value & 0xff0000)) >> 8) | ((value & 0xff000000) >> 24))
+
+#define DEBUG
+#ifdef DEBUG
+#define DBGPRT(format, args...)       {printf("[DEBUG] ");printf(format, ##args);putchar('\n');}
+#else
+#define DBGPRT(format, args...)       
+#endif
 
 
 void test(int a);
 
 uint32_t*   SHA1(uint32_t *digest, uint8_t *data, uint64_t data_length);
 uint8_t*    prepare_datatail(uint8_t data_tail[CHUNK_SIZE_BYTES + 8], uint64_t data_len, int no_tailbytes);
-bool        digest_chunk(uint32_t *hash_words, uint8_t *data, uint64_t *rem_data_bytes, uint8_t* data_tail, int* rem_tail_bytes);
+bool        digest_chunk(uint32_t *hash_words, uint8_t **data, uint64_t *rem_data_bytes, uint8_t *data_tail, int* rem_tail_bytes);
 
 void        print_sha(uint32_t *digest, bool append_newline);
