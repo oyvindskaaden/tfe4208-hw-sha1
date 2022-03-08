ca4593af507dd007eb0a896e0a29b5e4e76ac089
Øyvind Skaaden <oyvindskaaden@gmail.com>
2022-03-08 13:21:57 +0100

    Small test

--- c_impl/src/test_sha.c
+++ c_impl/src/test_sha.c
@@ -5,7 +5,15 @@
 
 int main(int argc, char const *argv[])
 {
-    test(2);
-    printf("Hello, World!\n");
+    uint32_t digest[5];
+
+    char data[] = "Hello, World!";
+
+    SHA1(digest, (uint8_t*)data, strlen(data));
+    
+    printf("Data:\n%s\n", data);
+
+    print_sha(digest, true);
+
     return 0;
 }
