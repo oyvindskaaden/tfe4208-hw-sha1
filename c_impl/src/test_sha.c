3512c360d865823a417d5f726fa6d1ca9d63a512
Øyvind Skaaden <oyvindskaaden@gmail.com>
2022-03-08 15:14:57 +0100

    long test

--- c_impl/src/test_sha.c
+++ c_impl/src/test_sha.c
@@ -7,12 +7,13 @@ int main(int argc, char const *argv[])
 {
     uint32_t digest[5];
 
-    char data[] = "Hello, World!";
+    char data[] = "0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef";
 
     SHA1(digest, (uint8_t*)data, strlen(data));
     
     printf("Data:\n%s\n", data);
 
+    printf("SHA1:\n");
     print_sha(digest, true);
 
     return 0;
